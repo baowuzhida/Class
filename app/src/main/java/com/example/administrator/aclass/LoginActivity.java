799d1173976cf4a55121be95602078d094fac6fa
Class <https://github.com/baowuzhida/Class.git>
2017-10-16 09:28:32 +0800

    Login commit

---
+++ app/src/main/java/com/example/administrator/aclass/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.aclass;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

b23346fb22d9fea58629bba221417c9c54e80d4d
Administrator <641419341@qq.com>
2017-10-19 10:36:26 +0800

    LoginActivity commit

--- app/src/main/java/com/example/administrator/aclass/LoginActivity.java
+++ app/src/main/java/com/example/administrator/aclass/LoginActivity.java
@@ -1,15 +1,35 @@
 package com.example.administrator.aclass;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Administrator on 2017/10/16.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button button;
+
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button.findViewById(R.id.submit);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.submit:
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+        }
+
     }
 }
